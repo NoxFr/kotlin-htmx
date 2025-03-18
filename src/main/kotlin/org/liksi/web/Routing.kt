6fead967908e921e28c4e26707a2316296824e98
DURAND, Mathieu <prestataire.0169783@ouest-france.fr>
2025-03-18 14:01:33 +0100

    ✨ Add beer detail dialog component and API routing for beer details

--- src/main/kotlin/org/liksi/web/Routing.kt
+++ src/main/kotlin/org/liksi/web/Routing.kt
@@ -4,7 +4,12 @@ import io.ktor.server.application.*
 import io.ktor.server.html.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
+import kotlinx.html.body
+import org.liksi.api.BeerService
+import org.liksi.web.components.beerDetailDialog
 import org.liksi.web.pages.index
+
+val beerService = BeerService()
 
 fun Application.configureRouting() {
     routing {
@@ -12,6 +17,19 @@ fun Application.configureRouting() {
         get("/") {
             call.respondHtml {
                 index()
+            }
+        }
+        get("/beer/{id}") {
+            val id = requireNotNull(call.parameters["id"]?.toIntOrNull()) {
+                "Invalid beer ID"
+            }
+            val beer = requireNotNull(beerService.getBeerById(id)) {
+                "Beer not found"
+            }
+            call.respondHtml {
+                body {
+                    beerDetailDialog(beer)
+                }
             }
         }
     }
