9602e2e19f2e096ade5c7bd3b8f04e497e1a0e7f
DURAND, Mathieu <prestataire.0169783@ouest-france.fr>
2025-03-18 11:00:16 +0100

    ✨ Add API routing configuration to the application module

--- src/main/kotlin/org/liksi/web/Application.kt
+++ src/main/kotlin/org/liksi/web/Application.kt
@@ -3,6 +3,7 @@ package org.liksi.web
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import org.liksi.api.configureApiRouting
 
 fun main() {
     embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -11,4 +12,5 @@ fun main() {
 
 fun Application.module() {
     configureRouting()
+    configureApiRouting()
 }
